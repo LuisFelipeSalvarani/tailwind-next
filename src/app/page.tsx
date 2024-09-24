626c569bfe07d5d6d737d903873ed2b873d187ce
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-09-24 10:45:03 -0300

    Seletores e Estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl">Hellow Tailwind</h1>
+      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+        Hellow Tailwind
+      </h1>
       <h2>Hellow Tailwind</h2>
       <p>Hellow Tailwind</p>
+      <button
+        disabled
+        type="button"
+        className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed"
+      >
+        Sign in
+      </button>
     </div>
   )
 }
