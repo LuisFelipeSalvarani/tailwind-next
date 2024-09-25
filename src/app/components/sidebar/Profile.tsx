7b3ebc09767e73b59b226b74de5aab310152f9fa
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-09-25 17:23:21 -0300

    Tema dark da sidebar

--- src/app/components/sidebar/Profile.tsx
+++ src/app/components/sidebar/Profile.tsx
@@ -10,15 +10,15 @@ export function Profile() {
         className="size-10 rounded-full"
       />
       <div className="flex flex-1 flex-col truncate">
-        <span className="font-semibold text-sm text-zinc-700">
+        <span className="font-semibold text-sm text-zinc-700 dark:text-zinc-100">
           Luis Felipe Salvarani
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           felipe_salvarani@hotmail.com
         </span>
       </div>
       <Button type="button" variant="ghost">
-        <LogOut className="size-5 text-zinc-500" />
+        <LogOut className="size-5" />
       </Button>
     </div>
   )
